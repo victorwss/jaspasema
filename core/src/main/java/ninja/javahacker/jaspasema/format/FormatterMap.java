983181f11c4d4a15b5846ea47d0da2e4fdb76f71
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2021-09-02 08:25:19 -0300

    Refactoring for better encapsulement.

---
+++ core/src/main/java/ninja/javahacker/jaspasema/format/FormatterMap.java
@@ -0,0 +1,135 @@
+package ninja.javahacker.jaspasema.format;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import lombok.NonNull;
+import lombok.experimental.PackagePrivate;
+import lombok.experimental.UtilityClass;
+import ninja.javahacker.reifiedgeneric.ReifiedGeneric;
+
+/**
+ * @author Victor Williams Stafusa da Silva
+ */
+@UtilityClass
+@PackagePrivate
+@SuppressFBWarnings("FII_USE_METHOD_REFERENCE")
+public class FormatterMap {
+
+    public static final Map<ReifiedGeneric<?>, FormatterFunction<?>> FORMAT_MAP = Map.ofEntries(
+            Map.entry(ReifiedGeneric.of(boolean.class), of((Boolean b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Boolean.class), of((Boolean b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(byte.class), of((Byte b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Byte.class), of((Byte b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(short.class), of((Short b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Short.class), of((Short b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(int.class), of((Integer b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Integer.class), of((Integer b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(long.class), of((Long b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Long.class), of((Long b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(float.class), of((Float b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Float.class), of((Float b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(double.class), of((Double b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(Double.class), of((Double b) -> b.toString())),
+            Map.entry(ReifiedGeneric.of(String.class), of(String::toString))
+    );
+
+    public static final Map<ReifiedGeneric<?>, Function<DateTimeFormatter, ? extends FormatterFunction<?>>> FORMATTER_DT_MAP =
+            Map.ofEntries(
+                    Map.entry(ReifiedGeneric.of(LocalDate.class), dtf -> of((LocalDate v) -> v.format(dtf))),
+                    Map.entry(ReifiedGeneric.of(LocalDateTime.class), dtf -> of((LocalDateTime v) -> v.format(dtf))),
+                    Map.entry(ReifiedGeneric.of(LocalTime.class), dtf -> of((LocalTime v) -> v.format(dtf))),
+                    Map.entry(ReifiedGeneric.of(Year.class), dtf -> of((Year v) -> v.format(dtf))),
+                    Map.entry(ReifiedGeneric.of(YearMonth.class), dtf -> of((YearMonth v) -> v.format(dtf)))
+            );
+
+    public static final Map<ReifiedGeneric<?>, ParseFunction<?>> PARSE_MAP = Map.ofEntries(
+            Map.entry(ReifiedGeneric.of(boolean.class), parse(FormatterMap::booleanParse)),
+            Map.entry(ReifiedGeneric.of(Boolean.class), parse(FormatterMap::booleanParse)),
+            Map.entry(ReifiedGeneric.of(byte.class), parse(Byte::valueOf)),
+            Map.entry(ReifiedGeneric.of(Byte.class), parse(Byte::valueOf)),
+            Map.entry(ReifiedGeneric.of(short.class), parse(Short::valueOf)),
+            Map.entry(ReifiedGeneric.of(Short.class), parse(Short::valueOf)),
+            Map.entry(ReifiedGeneric.of(int.class), parse(Integer::valueOf)),
+            Map.entry(ReifiedGeneric.of(Integer.class), parse(Integer::valueOf)),
+            Map.entry(ReifiedGeneric.of(long.class), parse(Long::valueOf)),
+            Map.entry(ReifiedGeneric.of(Long.class), parse(Long::valueOf)),
+            Map.entry(ReifiedGeneric.of(float.class), parse(Float::valueOf)),
+            Map.entry(ReifiedGeneric.of(Float.class), parse(Float::valueOf)),
+            Map.entry(ReifiedGeneric.of(double.class), parse(Double::valueOf)),
+            Map.entry(ReifiedGeneric.of(Double.class), parse(Double::valueOf)),
+            Map.entry(ReifiedGeneric.of(BigInteger.class), parse(BigInteger::new)),
+            Map.entry(ReifiedGeneric.of(BigDecimal.class), parse(BigDecimal::new)),
+            Map.entry(ReifiedGeneric.of(String.class), new ParseFunction<String>() {
+                @Override
+                public <X extends Throwable> String parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) {
+                    return s;
+                }
+            })
+    );
+
+    public static final Map<ReifiedGeneric<?>, Function<DateTimeFormatter, ? extends ParseFunction<?>>> PARSE_DT_MAP = Map.ofEntries(
+            Map.entry(ReifiedGeneric.of(LocalDate.class), parseDate(LocalDate::parse)),
+            Map.entry(ReifiedGeneric.of(LocalDateTime.class), parseDate(LocalDateTime::parse)),
+            Map.entry(ReifiedGeneric.of(LocalTime.class), parseDate(LocalTime::parse)),
+            Map.entry(ReifiedGeneric.of(Year.class), parseDate(Year::parse)),
+            Map.entry(ReifiedGeneric.of(YearMonth.class), parseDate(YearMonth::parse))
+    );
+
+    @NonNull
+    private static <E> FormatterFunction<E> of(@NonNull Function<E, String> func) {
+        return v -> v == null ? null : func.apply(v);
+    }
+
+    private static boolean booleanParse(@NonNull String s) {
+        switch (s) {
+            case "true": return true;
+            case "false": return false;
+            default: throw new IllegalArgumentException(s);
+        }
+    }
+
+    @NonNull
+    private static <E> ParseFunction<E> parse(@NonNull Function<String, E> func) {
+        return new ParseFunction<>() {
+            @Nullable
+            @Override
+            @SuppressFBWarnings("LEST_LOST_EXCEPTION_STACK_TRACE")
+            public <X extends Throwable> E parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) throws X {
+                if ("null".equals(s) || s.isEmpty()) return null;
+                try {
+                    return func.apply(s);
+                } catch (Exception e) {
+                    throw onError.apply(e);
+                }
+            }
+        };
+    }
+
+    private static <E> Function<DateTimeFormatter, ParseFunction<E>> parseDate(@NonNull BiFunction<String, DateTimeFormatter, E> func) {
+        return format -> new ParseFunction<>() {
+            @Nullable
+            @Override
+            @SuppressFBWarnings("LEST_LOST_EXCEPTION_STACK_TRACE")
+            public <X extends Throwable> E parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) throws X {
+                if ("null".equals(s) || s.isEmpty()) return null;
+                try {
+                    return func.apply(s, format);
+                } catch (DateTimeException e) {
+                    throw onError.apply(e);
+                }
+            }
+        };
+    }
+}
--- core/src/main/java/ninja/javahacker/jaspasema/format/ParseFunction.java
+++ core/src/main/java/ninja/javahacker/jaspasema/format/ParseFunction.java
@@ -2,18 +2,8 @@ package ninja.javahacker.jaspasema.format;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Year;
-import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
-import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import lombok.NonNull;
@@ -27,82 +17,8 @@ public interface ParseFunction<E> {
     @Nullable
     public <X extends Throwable> E parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) throws X;
 
-    @NonNull
-    private static <E> ParseFunction<E> of(@NonNull Function<String, E> func) {
-        return new ParseFunction<>() {
-            @Nullable
-            @Override
-            @SuppressFBWarnings("LEST_LOST_EXCEPTION_STACK_TRACE")
-            public <X extends Throwable> E parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) throws X {
-                if ("null".equals(s) || s.isEmpty()) return null;
-                try {
-                    return func.apply(s);
-                } catch (Exception e) {
-                    throw onError.apply(e);
-                }
-            }
-        };
-    }
-
-    private static <E> Function<DateTimeFormatter, ParseFunction<E>> ofDate(@NonNull BiFunction<String, DateTimeFormatter, E> func) {
-        return format -> new ParseFunction<>() {
-            @Nullable
-            @Override
-            @SuppressFBWarnings("LEST_LOST_EXCEPTION_STACK_TRACE")
-            public <X extends Throwable> E parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) throws X {
-                if ("null".equals(s) || s.isEmpty()) return null;
-                try {
-                    return func.apply(s, format);
-                } catch (DateTimeException e) {
-                    throw onError.apply(e);
-                }
-            }
-        };
-    }
-
-    private static boolean booleanParse(@NonNull String s) {
-        switch (s) {
-            case "true": return true;
-            case "false": return false;
-            default: throw new IllegalArgumentException(s);
-        }
-    }
-
-    public static final Map<ReifiedGeneric<?>, ParseFunction<?>> MAP = Map.ofEntries(
-            Map.entry(ReifiedGeneric.of(boolean.class), ParseFunction.of(ParseFunction::booleanParse)),
-            Map.entry(ReifiedGeneric.of(Boolean.class), ParseFunction.of(ParseFunction::booleanParse)),
-            Map.entry(ReifiedGeneric.of(byte.class), ParseFunction.of(Byte::valueOf)),
-            Map.entry(ReifiedGeneric.of(Byte.class), ParseFunction.of(Byte::valueOf)),
-            Map.entry(ReifiedGeneric.of(short.class), ParseFunction.of(Short::valueOf)),
-            Map.entry(ReifiedGeneric.of(Short.class), ParseFunction.of(Short::valueOf)),
-            Map.entry(ReifiedGeneric.of(int.class), ParseFunction.of(Integer::valueOf)),
-            Map.entry(ReifiedGeneric.of(Integer.class), ParseFunction.of(Integer::valueOf)),
-            Map.entry(ReifiedGeneric.of(long.class), ParseFunction.of(Long::valueOf)),
-            Map.entry(ReifiedGeneric.of(Long.class), ParseFunction.of(Long::valueOf)),
-            Map.entry(ReifiedGeneric.of(float.class), ParseFunction.of(Float::valueOf)),
-            Map.entry(ReifiedGeneric.of(Float.class), ParseFunction.of(Float::valueOf)),
-            Map.entry(ReifiedGeneric.of(double.class), ParseFunction.of(Double::valueOf)),
-            Map.entry(ReifiedGeneric.of(Double.class), ParseFunction.of(Double::valueOf)),
-            Map.entry(ReifiedGeneric.of(BigInteger.class), ParseFunction.of(BigInteger::new)),
-            Map.entry(ReifiedGeneric.of(BigDecimal.class), ParseFunction.of(BigDecimal::new)),
-            Map.entry(ReifiedGeneric.of(String.class), new ParseFunction<String>() {
-                @Override
-                public <X extends Throwable> String parse(@NonNull Function<? super Throwable, X> onError, @NonNull String s) {
-                    return s;
-                }
-            })
-    );
-
-    public static final Map<ReifiedGeneric<?>, Function<DateTimeFormatter, ? extends ParseFunction<?>>> DT_MAP = Map.ofEntries(
-            Map.entry(ReifiedGeneric.of(LocalDate.class), ofDate(LocalDate::parse)),
-            Map.entry(ReifiedGeneric.of(LocalDateTime.class), ofDate(LocalDateTime::parse)),
-            Map.entry(ReifiedGeneric.of(LocalTime.class), ofDate(LocalTime::parse)),
-            Map.entry(ReifiedGeneric.of(Year.class), ofDate(Year::parse)),
-            Map.entry(ReifiedGeneric.of(YearMonth.class), ofDate(YearMonth::parse))
-    );
-
     public static boolean accepts(@NonNull ReifiedGeneric<?> target) {
-        return MAP.get(target) != null || DT_MAP.get(target) != null;
+        return FormatterMap.PARSE_MAP.get(target) != null || FormatterMap.PARSE_DT_MAP.get(target) != null;
     }
 
     @NonNull
@@ -117,14 +33,14 @@ public interface ParseFunction<E> {
             throws W, X, Y, Z
     {
         @SuppressWarnings("unchecked")
-        var x = (ParseFunction<E>) MAP.get(target);
+        var x = (ParseFunction<E>) FormatterMap.PARSE_MAP.get(target);
         if (x != null) {
             if (!dateFormat.isEmpty()) throw nonDateWithFormat.get();
             return x;
         }
 
         @SuppressWarnings("unchecked")
-        var d = (Function<DateTimeFormatter, ParseFunction<E>>) DT_MAP.get(target);
+        var d = (Function<DateTimeFormatter, ParseFunction<E>>) FormatterMap.PARSE_DT_MAP.get(target);
         if (d == null) throw unmappeableTarget.get();
 
         if (dateFormat.isEmpty()) throw dateWithoutFormat.get();
